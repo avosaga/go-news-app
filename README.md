7a38678a101949734b22faa076d78b238abf25cb
Alvaro Sanchez <alvaro.sanchez@hootsuite.com>
2022-11-06 16:08:26 -0700

    Change README.md and add log messages in main

---
+++ README.md
@@ -0,0 +1,20 @@
+# Go News App
+
+Small web app to get news about certain topic you want to search for
+
+## Requirements
+
+The only requirement is to set up an API key at [news.org](https://newsapi.org/) then paste it in `config/app.env`
+as `API_KEY=<YOUR_KEY>`
+
+## Run locally
+
+```
+go build -o go-news-app cmd/web/main.go && ./go-news-app
+```
+
+## Demo
+
+Go to []()
+
+
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,20 +4,25 @@ import (
 	"github.com/avosaga/go-news-app/api"
 	"github.com/avosaga/go-news-app/config"
 	"github.com/avosaga/go-news-app/pkg/handlers"
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
+	log.Println("Getting config...")
 	conf := config.GetConfig()
 
+	log.Println("Creating API Client...")
 	client := &http.Client{Timeout: 10 * time.Second}
 	newsApi := api.NewClient(client, conf.ApyKey, 15)
 
+	log.Println("Running server...")
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", handlers.AssetsHandler)
 	mux.HandleFunc("/", handlers.IndexHandler)
 	mux.HandleFunc("/search", handlers.SearchHandler(newsApi))
 
+	log.Printf("Server up and running at https://localhost%s", conf.Port)
 	http.ListenAndServe(conf.Port, mux)
 }
